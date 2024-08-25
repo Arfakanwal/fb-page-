5878fe7658a1bdcde35e1cbf23be7cbfa44020b7
Arfakanwal <Arfakanwal170@gmail.com>
2024-08-25 12:04:24 +0500

    not completed

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('signupForm').addEventListener('submit' , function(event){
+    event.preventDefault();
+
+    var email = document.getElementById('email').value.trim();
+    var password = document.getElementById('password').value.trim();
+
+   
+});
